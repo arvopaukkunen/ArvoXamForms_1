b7fe7457c18c952abcd85bd4e3f0241e19b6ad8f
arvopaukkunen <arvosoft@hotmail.com>
2016-12-14 21:11:37 +0200

    Xamarin starter project test
    Just another test

---
+++ ArvoXamForms_1/ArvoXamForms_1Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ArvoXamForms_1
+{
+	public partial class ArvoXamForms_1Page : ContentPage
+	{
+		public ArvoXamForms_1Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
